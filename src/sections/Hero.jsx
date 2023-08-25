c52481c81b46d33a54b455ee83a16a3de645df21
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-25 05:11:43 +0100

    hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,11 @@
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="w-full flex-col xl:flex-row justify-center min-h-screen gap-10 max-container">
+      <div className="relative xl:w-2/5 pt-28 max-xl:padding-x flex flex-col justify-center items-start w-full">
+        <p className="">Our Summer Collection</p>
+        <h1>The New Arrival</h1>
+      </div>
+    </section>
   )
 }
 
