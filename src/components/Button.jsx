43ca2aa1958ac9d3858d198f8d3438835b7ba189
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-25 19:13:54 +0100

    superQuality

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,16 @@
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL }) => {
   return (
-    <button className="flex justify-center items-center gap-2 py-4 px-7 border font-montserrat text-lg leading-none text-white rounded-full bg-coral-red cursor-pointer border-coral-red">{label}
-    <img src={iconURL} alt="arror right icon" className="ml-2 rounded-full w-5 h-5" />
+    <button className="flex justify-center items-center gap-2 py-4 px-7 border font-montserrat text-lg leading-none text-white rounded-full bg-coral-red cursor-pointer border-coral-red">
+      {label}
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arror right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -2,22 +2,29 @@ import Button from "../components/Button";
 
 const SuperQuality = () => {
   return (
-    <section id="about-us" className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
       <div className="flex flex-1 flex-col">
         <h2 className="font-palanquin capitalize text-4xl font-bol lg:max-w-lg">
-           We Provide You 
+          We Provide You
           <span className="text-coral-red"> Super Quality</span> Shoes
         </h2>
         <p className="info-text mt-4 lg:max-w-lg">
-          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your lifestyle, providing you with unmatched quality, innovation, and a touch of elegance.
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your lifestyle, providing you with unmatched
+          quality, innovation, and a touch of elegance.
         </p>
         <p className="mt-6 lg:max-w-lg info-text">
           Our dedication to detail and excellence ensures your satisfaction.
         </p>
-        <Button label="View Details" />
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SuperQuality
+export default SuperQuality;
