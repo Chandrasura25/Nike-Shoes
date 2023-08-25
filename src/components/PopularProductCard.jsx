df038923efdd2fc1a09aadda2d2c80ba2a114844
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-25 18:49:31 +0100

    popular products card

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,7 +1,17 @@
-const PopularProductCard = () => {
-  return (
-    <div>PopularProductCard</div>
-  )
-}
+import { star } from "../assets/icons";
 
-export default PopularProductCard
+const PopularProductCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="flex gap-2.5 justify-start mt-8">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="font-montserrat text-slate-gray text-xl leading-normal">(4.5)</p>
+      </div>
+      <h3 className="">{name}</h3>
+        <p className="font-palanquin font-bold text-lg">{price}</p>
+    </div>
+  );
+};
+
+export default PopularProductCard;
