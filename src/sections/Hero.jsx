76520cb16596d6831440c56ef35b1ed2391cf67e
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-25 05:48:56 +0100

    hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,29 +1,43 @@
-import { arrowRight } from "../assets/icons"
-import Button from "../components/Button"
-import { statistics } from "../constants"
+import { arrowRight } from "../assets/icons";
+import { bigShoe1 } from "../assets/images";
+import Button from "../components/Button";
+import { statistics } from "../constants";
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container">
-      <div className="relative xl:w-2/5 pt-28 max-xl:padding-x flex flex-col justify-center items-start w-full">
-        <p className="">Our Summer Collection</p>
-        <h1>
-          <span>The New Arrival</span>
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat">Our Summer Collection</p>
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] font-bold max-sm:leading-[82]">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
           <br />
-          <span>Nike</span>Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
-        <p className="">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
         <Button label="Show now" iconURL={arrowRight} />
         <div className="flex w-full flex-wrap items-start justify-start mt-20 gap-16">
-          {statistics.map((stat,index)=>(
-            <div className="" key={index}>
-              <p>{stat.value}</p>
-              <p>{stat.label}</p>
+          {statistics.map((stat) => (
+            <div key={stat.label}>
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
             </div>
           ))}
         </div>
       </div>
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen">
+        <img src={bigShoe1} alt="Shoe Collection" width={610} height={500} className="object-contain relative z-10" />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
