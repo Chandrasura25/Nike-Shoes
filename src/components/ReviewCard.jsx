1086cf2adfef9ed0c400ac4282c2a53549175492
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 11:43:51 +0100

    customer reviews cards

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,7 +1,27 @@
-const ReviewCard = () => {
-  return (
-    <div>ReviewCard</div>
-  )
-}
+import { star } from "../assets/icons";
 
-export default ReviewCard
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"
+      />
+      <p className="">{feedback}</p>
+      <div className="">
+        <img
+          src={star}
+          alt="rating"
+          width={24}
+          height={24}
+          className="object-contain m-0"
+        />
+        <p className="text-coral-red">({rating})</p>
+      </div>
+        <h3 className="text-coral-red">{customerName}</h3>
+    </div>
+  );
+};
+
+export default ReviewCard;
