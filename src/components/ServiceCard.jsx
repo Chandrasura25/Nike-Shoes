f9ef0b89144823aabf1696ff19e112b7a2e1a63e
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 10:57:46 +0100

    service card done

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,13 +1,15 @@
-const ServiceCard = ({imgURL, label, subtext}) => {
+const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
-    <div className="flex sm:w-[350px] w-full rounded-[20px] sm:min-w-[350px] shadow-3xl px-10 py-16">
-      <div className="flex w-11 h-11 justify-center items-center bg-coral-red">
+    <div className="flex-1 sm:w-[350px] w-full rounded-[20px] sm:min-w-[350px] shadow-3xl px-10 py-16 transition-all">
+      <div className="flex w-11 h-11 justify-center items-center bg-coral-red rounded-full">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
-      <h3 className="font-palanquin leading-normal font-bold text-3xl mt-5">{label}</h3>
-      <p className="mt-3 break-words">{subtext}</p>
+      <h3 className="font-palanquin leading-normal font-bold text-3xl mt-5">
+        {label}
+      </h3>
+      <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">{subtext}</p>
     </div>
-  )
-}
+  );
+};
 
-export default ServiceCard
+export default ServiceCard;
