c92dcd3ab168a1b2cf411d2b7183a5998bf2acdc
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 10:52:20 +0100

    service card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,6 +1,10 @@
-const ServiceCard = () => {
+const ServiceCard = ({imgURL, label, subtext}) => {
   return (
-    <div>ServiceCard</div>
+    <div className="flex sm:w-[350px] w-full rounded-[20px] sm:min-w-[350px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 justify-center items-center bg-coral-red">
+        <img src={imgURL} alt={label} width={24} height={24} />
+      </div>
+    </div>
   )
 }
 
