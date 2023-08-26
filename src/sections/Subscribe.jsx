52f41037dd20afb9141b73d69d998c20cc8e4e3a
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 11:52:58 +0100

    subscribe

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,23 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+        Sign Up for
+        <span className="text-coral-red"> Updates</span> & Newsletter
+      </h3>
+      <div className="">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="">
+          <Button label="Sign Up"/>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Subscribe
+export default Subscribe;
