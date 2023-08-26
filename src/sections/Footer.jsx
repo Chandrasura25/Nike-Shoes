8c18fc5d52c39611d71ebfac7a302406ef123ef4
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2023-08-26 12:20:22 +0100

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,17 +1,39 @@
-import { footerLogo } from "../assets/images"
+import { footerLogo } from "../assets/images";
+import { socialMedia } from "../constants";
 
 const Footer = () => {
   return (
     <footer className="max-container">
       <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
         <div className="flex flex-col items-start">
-          <a href="/" className="">
-            <img src={footerLogo} alt="logo" width={150} height={46} className="object-contain" />
+          <a href="/">
+            <img
+              src={footerLogo}
+              alt="logo"
+              width={150}
+              height={46}
+              className="object-contain"
+            />
           </a>
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            your perfect size in store. Get Rewards.
+          </p>
+          <div className="flex items-center gap-5 mt-8">
+            {socialMedia.map((icon) => (
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                key={icon}
+              >
+                <img src={icon.src} alt={icon.alt} />
+              </div>
+            ))}
+          </div>
         </div>
+        <div className=""></div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
